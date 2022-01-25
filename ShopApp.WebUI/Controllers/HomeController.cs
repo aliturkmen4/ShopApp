ab29b01cc29b2324289c5c10d128272a5885a4b4
Ali <alym11731996@gmail.com>
2022-01-25 23:38:28 +0300

    Introduction

---
+++ ShopApp.WebUI/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopApp.WebUI.Controllers
+{
+    public class HomeController:Controller //aspnetcore.mvc adresinden aldım controllerı
+    {
+        //localhost:5000/home/index
+        public string Index() //action methodu
+        {
+            return "home/index";
+        }
+
+        //localhost:5000/home/about
+        public string About() //action methodu
+        {
+            return "home/about";
+        }
+    }
+}
---
+++ ShopApp.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopApp.WebUI.Controllers
+{
+    public class ProductController:Controller
+    {
+        public string Index() //action methodu
+        {
+            return "product/index";
+        }
+
+        //localhost:5000/product/list
+        public string List() //action methodu
+        {
+            return "product/list";
+        }
+
+        //localhost:5000/product/details
+        public string Details(int id) //action methodu
+        {
+            return "product/details/"+id;
+        }
+    }
+}
