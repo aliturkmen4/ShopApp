e257061561e43abf786575b93509e1eb43820f37
Ali <alym11731996@gmail.com>
2022-01-27 00:30:21 +0300

    model layer added

--- ShopApp.WebUI/Controllers/ProductController.cs
+++ ShopApp.WebUI/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ShopApp.WebUI.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,9 +21,23 @@ namespace ShopApp.WebUI.Controllers
         }
 
         //localhost:5000/product/details
-        public string Details(int id) //action methodu
+        public IActionResult Details(int id) //action methodu
         {
-            return "product/details/"+id;
+            //name:"samsung s6"
+            //prive: 3000
+            //description: "iyi telefon"
+
+            //ViewBag.Name = "samsung s6";
+            //ViewBag.Price = 3000;
+            //ViewBag.Description = "İyi telefon";
+
+            var p = new Product();
+
+            p.Name="Samsung s6";
+            p.Price = 3000;
+            p.Description = "İyi telefon";
+
+            return View(p); //p nesnesini view e gönderdim!
         }
     }
 }
